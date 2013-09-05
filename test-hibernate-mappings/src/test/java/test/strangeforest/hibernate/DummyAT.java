a1709fdbaca1fe7ad0c06272e18522a5008829fe
Mileta Cekovic <mileta.cekovic@gtechg2.com>
2013-09-05 10:58:00 +0200

    tweaks

---
+++ test-hibernate-mappings/src/test/java/test/strangeforest/hibernate/DummyAT.java
@@ -0,0 +1,13 @@
+package test.strangeforest.hibernate;
+
+import org.testng.annotations.*;
+
+import static org.testng.Assert.*;
+
+public class DummyAT {
+
+	@Test
+	public void dummyTest() {
+		assertTrue(true);
+	}
+}
