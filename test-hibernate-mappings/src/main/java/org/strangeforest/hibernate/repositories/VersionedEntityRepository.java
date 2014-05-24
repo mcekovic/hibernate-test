c651b2ec221263b223d4b3a05e00c412f3eec85b
Mileta Cekovic <mileta.cekovic@gtech.com>
2014-05-24 17:15:44 +0200

    Locking performance tests

--- test-hibernate-mappings/src/main/java/org/strangeforest/hibernate/repositories/VersionedEntityRepository.java
+++ test-hibernate-mappings/src/main/java/org/strangeforest/hibernate/repositories/VersionedEntityRepository.java
@@ -1,6 +1,9 @@
 package org.strangeforest.hibernate.repositories;
 
+import javax.persistence.*;
+
 import org.springframework.stereotype.*;
+import org.springframework.transaction.annotation.*;
 import org.strangeforest.hibernate.entities.*;
 
 @Repository
@@ -8,5 +11,20 @@ public class VersionedEntityRepository extends JPARepository<VersionedEntity> {
 
 	public VersionedEntityRepository() {
 		super(VersionedEntity.class);
+	}
+
+	@Transactional
+	public void changeName(long id, String name) {
+		em.find(entityClass, id, LockModeType.NONE).setName(name);
+	}
+
+	@Transactional
+	public void changeNameOptimistic(long id, String name) {
+		em.find(entityClass, id, LockModeType.OPTIMISTIC).setName(name);
+	}
+
+	@Transactional
+	public void changeNamePessimistic(long id, String name) {
+		em.find(entityClass, id, LockModeType.PESSIMISTIC_WRITE).setName(name);
 	}
 }
--- test-hibernate-mappings/src/test/java/test/strangeforest/hibernate/VersionedEntityIT.java
+++ test-hibernate-mappings/src/test/java/test/strangeforest/hibernate/VersionedEntityIT.java
@@ -1,5 +1,6 @@
 package test.strangeforest.hibernate;
 
+import java.util.*;
 import javax.persistence.*;
 
 import org.springframework.beans.factory.annotation.*;
@@ -17,9 +18,12 @@ public class VersionedEntityIT extends AbstractTestNGSpringContextTests {
 
 	@Autowired private VersionedEntityRepository entities;
 	private long entityId;
+	private Random rnd = new Random();
 
 	private static final String ENTITY_NAME = "Entity";
 	private static final String DETAIL_NAME = "Detail";
+	private static final int UPDATE_WARM_COUNT = 1;
+	private static final int UPDATE_COUNT      = 2;
 
 	@Test
 	public void entityIsCreated() {
@@ -33,7 +37,7 @@ public class VersionedEntityIT extends AbstractTestNGSpringContextTests {
 	@Test(dependsOnMethods = "entityIsCreated")
 	public void entityIsUpdated() {
 		VersionedEntity entity = getEntity();
-		entity.setName(ENTITY_NAME + "2");
+		entity.setName(ENTITY_NAME + '!');
 		entities.save(entity);
 
 		assertThat(getEntity().getVersion(), is(equalTo(1)));
@@ -50,7 +54,43 @@ public class VersionedEntityIT extends AbstractTestNGSpringContextTests {
 		assertThat(savedEntity.getVersion(), is(equalTo(2)));
 	}
 
-	@Test(dependsOnMethods = "detailIsAdded", expectedExceptions = EntityNotFoundException.class)
+	@Test(dependsOnMethods = "detailIsAdded")
+	public void warmEntityNameIsChanged() {
+		for (int i = 0; i < UPDATE_WARM_COUNT; i++)
+			entities.changeName(entityId, ENTITY_NAME + rnd.nextLong());
+	}
+
+	@Test(dependsOnMethods = "warmEntityNameIsChanged")
+	public void warmEntityNameIsChangedOptimistic() {
+		for (int i = 0; i < UPDATE_WARM_COUNT; i++)
+			entities.changeNameOptimistic(entityId, ENTITY_NAME + rnd.nextLong());
+	}
+
+	@Test(dependsOnMethods = "warmEntityNameIsChangedOptimistic")
+	public void warmEntityNameIsChangedPessimistic() {
+		for (int i = 0; i < UPDATE_WARM_COUNT; i++)
+			entities.changeNamePessimistic(entityId, ENTITY_NAME + rnd.nextLong());
+	}
+
+	@Test(dependsOnMethods = "warmEntityNameIsChangedPessimistic")
+	public void entityNameIsChanged() {
+		for (int i = 0; i < UPDATE_COUNT; i++)
+			entities.changeName(entityId, ENTITY_NAME + rnd.nextLong());
+	}
+
+	@Test(dependsOnMethods = "entityNameIsChanged")
+	public void entityNameIsChangedOptimistic() {
+		for (int i = 0; i < UPDATE_COUNT; i++)
+			entities.changeNameOptimistic(entityId, ENTITY_NAME + rnd.nextLong());
+	}
+
+	@Test(dependsOnMethods = "entityNameIsChangedOptimistic")
+	public void entityNameIsChangedPessimistic() {
+		for (int i = 0; i < UPDATE_COUNT; i++)
+			entities.changeNamePessimistic(entityId, ENTITY_NAME + rnd.nextLong());
+	}
+
+	@Test(dependsOnMethods = "entityNameIsChangedPessimistic", expectedExceptions = EntityNotFoundException.class)
 	public void entityIdDeleted() {
 		entities.deleteById(entityId);
 		getEntity();
